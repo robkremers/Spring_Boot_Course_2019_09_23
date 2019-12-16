90582ef62b479d7b6bb595102210f6c024c9d97c
Rob Kremers <robkremers@hotmail.com>
2019-12-16 23:08:38 +0100

    Removed /AppConfigAutowired.java

---
+++ exercise4/src/main/java/customers/EmailSender.java
@@ -0,0 +1,31 @@
+package customers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSender implements IEmailSender {
+	private String outgoingMailServer = "smtp.acme.com";
+	private ILogger logger;
+	
+	@Value(value = "smtp.mailserver.com")
+	private String emailServer;
+	
+	public EmailSender() {
+	}
+	
+	@Autowired
+	public EmailSender(ILogger logger) {
+		this.logger = logger;
+	}
+
+	public String getOutgoingMailServer() {
+		return outgoingMailServer;
+	}
+
+	public void sendEmail (String email, String message){
+		System.out.println("EmailSender: sending '"+message+"' to "+email + " using mailserver " + emailServer );
+		logger.log("Email is sent: message= "+message +" , emailaddress ="+ email  );
+	}
+}
